6ca53a624c7ff913c8c9383825bbb29e704564fb
Luz Elena Limón de la Cruz <luzelenalimondelacruz123@gmail.com>
2022-05-12 20:16:23 -0500

    remove a line 9

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ function testing() {
 }
 function developer() {
     console.log("I am developer a new project :)");
-    console.log("Hello World bug fixeed!");
 }
 function bigFunction(){
-    
+
 }
