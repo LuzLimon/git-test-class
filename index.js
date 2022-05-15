5175b07807a4b342f51530f3371c560baa439d04
Luz Elena Limón de la Cruz <luzelenalimondelacruz123@gmail.com>
2022-05-15 01:22:57 -0500

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function doSomething() {
     console.log("Hello World!");
 }
 function developer() {
-    console.log("I am developer");
+    console.log("I am developer!");
 }
 function testing() {
     console.log("testing");
